e99898e5d315bb85a7ae6d982a42fae1925abbb3
WalterSamuelBorgellas <borgellassamuel@gmail.com>
2023-11-07 06:48:07 -0500

    Message de commit

---
+++ koupe_espas.py
@@ -0,0 +1,4 @@
+chen =input ("Antre done yo nan chen karakte sa : \n")
+lis = chen.split() 
+for lis_san_espas in lis:
+    print(lis_san_espas)
--- mete_karakte_an_miniskil.py
+++ mete_karakte_an_miniskil.py
@@ -2,4 +2,4 @@
 
 chen_karakte = input("Antre done yo nan chen karakte sa : \n")
 chen_karakte_miniskil = chen_karakte.lower()
-print("Chen miniskil la se " +chen_karakte_miniskil)
+print("Chen miniskil la se " +chen_karakte_miniskil) 
